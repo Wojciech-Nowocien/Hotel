0184e605a9693fda5d1a8b3c9a0d1c928663a142
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-13 19:03:52 +0100

    Add User.java

---
+++ src/main/java/model/User.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class User {
+    private final String login;
+    private final String passwordHash;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.passwordHash = hash(password);
+    }
+
+    private String hash(String text) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] bytes = digest.digest(text.getBytes(StandardCharsets.UTF_8));
+
+            return new BigInteger(1, bytes).toString(16);
+        } catch (NoSuchAlgorithmException e) {
+            return text;
+        }
+    }
+
+    public boolean login(String password) {
+        return hash(password).equals(passwordHash);
+    }
+}
