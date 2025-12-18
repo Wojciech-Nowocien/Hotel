a08eb7b019588c67530e34e9abbab9bce5b2de75
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-18 11:28:29 +0100

    Add Event.java

---
+++ src/main/java/events/Event.java
@@ -0,0 +1,32 @@
+package events;
+
+import model.Admin;
+import model.Client;
+import model.Room;
+import model.User;
+
+public class Event {
+    private final Room room;
+    private final User user;
+
+    public Event(Room room, User user) {
+        this.room = room;
+        this.user = user;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public boolean isExecutorClient() {
+        return user instanceof Client;
+    }
+
+    public boolean isExecutorAdmin() {
+        return user instanceof Admin;
+    }
+}
