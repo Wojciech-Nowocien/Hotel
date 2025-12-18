5b016f3c435777e377d70183e31abbb04a9dfed6
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-18 12:03:23 +0100

    Add support for AdminEventType in AdminEvent.java

--- src/main/java/events/AdminEvent.java
+++ src/main/java/events/AdminEvent.java
@@ -1,10 +1,18 @@
 package events;
 
+import events.enums.AdminEventType;
 import model.Admin;
 import model.Room;
 
-public class AdminEvent extends Event{
-    public AdminEvent(Room room, Admin admin) {
+public class AdminEvent extends Event {
+    private final AdminEventType type;
+
+    public AdminEvent(Room room, Admin admin, AdminEventType type) {
         super(room, admin);
+        this.type = type;
+    }
+
+    public AdminEventType getType() {
+        return type;
     }
 }
