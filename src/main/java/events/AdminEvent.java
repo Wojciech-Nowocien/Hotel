7aa703f770a65366d3d7512fd4614405d9405559
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-20 18:41:51 +0100

    Implement abstract methods in AdminEvent.java

--- src/main/java/events/AdminEvent.java
+++ src/main/java/events/AdminEvent.java
@@ -1,18 +1,35 @@
 package events;
 
 import events.enums.AdminEventType;
+import events.enums.availability.AvailabilityImpact;
+import events.enums.availability.AvailabilityRequirement;
 import model.Admin;
 import model.Room;
 
 public class AdminEvent extends Event {
     private final AdminEventType type;
+    private final AvailabilityImpact impact;
+    private final AvailabilityRequirement requirement;
 
-    public AdminEvent(Room room, Admin admin, AdminEventType type) {
+    public AdminEvent(Room room, Admin admin, AdminEventType type, AvailabilityImpact impact,
+                      AvailabilityRequirement requirement) {
         super(room, admin);
         this.type = type;
+        this.impact = impact;
+        this.requirement = requirement;
     }
 
     public AdminEventType getType() {
         return type;
+    }
+
+    @Override
+    AvailabilityImpact getStatus() {
+        return impact;
+    }
+
+    @Override
+    AvailabilityRequirement getRequirement() {
+        return requirement;
     }
 }
