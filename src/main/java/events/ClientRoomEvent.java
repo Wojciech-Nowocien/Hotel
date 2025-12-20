fe0c93ef6a11436051369e229d6032d966881ef9
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-20 18:42:22 +0100

    Implement abstract methods in ClientRoomEvent.java

--- src/main/java/events/ClientRoomEvent.java
+++ src/main/java/events/ClientRoomEvent.java
@@ -1,18 +1,35 @@
 package events;
 
 import events.enums.ClientEventType;
+import events.enums.availability.AvailabilityImpact;
+import events.enums.availability.AvailabilityRequirement;
 import model.Client;
 import model.Room;
 
 public class ClientRoomEvent extends Event{
     private final ClientEventType type;
+    private final AvailabilityImpact impact;
+    private final AvailabilityRequirement requirement;
 
-    public ClientRoomEvent(Room room, Client client, ClientEventType type) {
+    public ClientRoomEvent(Room room, Client client, ClientEventType type, AvailabilityImpact impact,
+                           AvailabilityRequirement requirement) {
         super(room, client);
         this.type = type;
+        this.impact = impact;
+        this.requirement = requirement;
     }
 
     public ClientEventType getType() {
         return type;
+    }
+
+    @Override
+    AvailabilityImpact getStatus() {
+        return impact;
+    }
+
+    @Override
+    AvailabilityRequirement getRequirement() {
+        return requirement;
     }
 }
