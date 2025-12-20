4d122e009dffbdddbb0f6b0e8883aba3f15367b5
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-20 19:24:48 +0100

    Add getLastStatus() method in EventManager.java

--- src/main/java/events/EventManager.java
+++ src/main/java/events/EventManager.java
@@ -1,5 +1,9 @@
 package events;
 
+import events.enums.availability.AvailabilityImpact;
+import events.enums.availability.Status;
+import model.Room;
+
 import java.util.ArrayList;
 
 public class EventManager {
@@ -7,5 +11,17 @@ public class EventManager {
 
     public EventManager() {
         events = new ArrayList<>();
+    }
+
+    private Status getLastStatus(Room room) {
+        Event event = events.stream().filter(e ->
+                e.getStatusImpact() != AvailabilityImpact.NONE && e.getRoom().equals(room)
+        ).toList().getLast();
+
+        if (event.getStatusImpact() == AvailabilityImpact.AVAILABLE) {
+            return Status.AVAILABLE;
+        } else {
+            return Status.UNAVAILABLE;
+        }
     }
 }
