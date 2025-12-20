ce2b276e50d0e2cee77f94de6102deba03b69ec1
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-20 19:12:18 +0100

    Add a new method in AvailabilityRequirement.java

--- src/main/java/events/enums/availability/AvailabilityRequirement.java
+++ src/main/java/events/enums/availability/AvailabilityRequirement.java
@@ -3,5 +3,13 @@ package events.enums.availability;
 public enum AvailabilityRequirement {
     NONE,
     REQUIRE_AVAILABLE,
-    REQUIRE_UNAVAILABLE
+    REQUIRE_UNAVAILABLE;
+
+    public boolean isCompatibleWith(Status status) {
+        return switch (this) {
+            case NONE -> true;
+            case REQUIRE_AVAILABLE -> status == Status.AVAILABLE;
+            case REQUIRE_UNAVAILABLE -> status == Status.UNAVAILABLE;
+        };
+    }
 }
