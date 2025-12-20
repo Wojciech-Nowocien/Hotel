ef5d878922e20d1e5aba501afb2a886c4f562e9b
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-20 19:14:44 +0100

    Initially add EventManager.java

---
+++ src/main/java/events/EventManager.java
@@ -0,0 +1,11 @@
+package events;
+
+import java.util.ArrayList;
+
+public class EventManager {
+    private final ArrayList<Event> events;
+
+    public EventManager() {
+        events = new ArrayList<>();
+    }
+}
