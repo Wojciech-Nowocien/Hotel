a07a813ce53e2cdefbac9a6391f9f08b9a81c30f
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-23 18:52:29 +0100

    Finish LoginScreen.java

--- src/screens/LoginScreen.java
+++ src/screens/LoginScreen.java
@@ -1,18 +1,47 @@
 package screens;
 
+import exceptions.InvalidPasswordException;
+import exceptions.LoginNotFoundException;
 import managers.UserManager;
-import model.Hotel;
+import model.Admin;
 
 public class LoginScreen extends Screen {
     private final UserManager users;
+    private String login;
+    private String password;
 
-    public LoginScreen(Hotel hotel) {
-        super(hotel);
+    public LoginScreen() {
         this.users = hotel.getUsers();
+        login = "";
+        password = "";
     }
 
     @Override
     public void render() {
+        System.out.println("Ekran logowania\n");
+        System.out.println("Podaj swój login:");
+        login = INPUT.nextLine();
+        System.out.println("Podaj swoje hasło:");
+        password = INPUT.nextLine();
 
+        try {
+            users.login(login, password);
+            if (users.getCurrentUser() instanceof Admin) {
+                System.out.println("\nZalogowano pomyślnie jako administrator. Przenoszenie do panelu administratora...\n");
+                changeScreen(new AdminScreen());
+            } else {
+                System.out.println("\nZalogowano pomyślnie jako klient. Przenoszenie do panelu klienta...\n");
+                changeScreen(new ClientScreen());
+            }
+        } catch (LoginNotFoundException loginException) {
+            System.out.println("Błąd: użytkownik o podanym loginie nie istnieje!");
+            System.out.println("To może być Twój błąd, lub takie konto wogule nie istnieje.");
+            System.out.println("Jeśli jesteś klientem, może spróbuj się zarejestrować?");
+            System.out.println("Jeśli jesteś administratorem skontaktuj się z innym z nich. Być może źle wprowadził Twój login?");
+            changeScreen(new WelcomeScreen());
+        } catch (InvalidPasswordException passwordException) {
+            System.out.println("Błąd: niepoprawne hasło dla użytkownika " + login + "!");
+            changeScreen(new WelcomeScreen());
+        }
     }
 }
