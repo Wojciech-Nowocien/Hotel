788668042320c40f04ba78cae18ea1fbf6539caf
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-23 17:57:26 +0100

    Simplify Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import model.Hotel;
 import screens.WelcomeScreen;
 
-public class Main {
-    static void main() {
-        Hotel hotel = new Hotel();
-        WelcomeScreen screen = new WelcomeScreen(hotel);
-        hotel.render(screen);
-    }
+void main() {
+    Hotel hotel = new Hotel();
+    WelcomeScreen screen = new WelcomeScreen(hotel);
+    hotel.render(screen);
 }
