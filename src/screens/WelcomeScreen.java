30abadf8c660edf89962c7cfcd4690eec0a2a023
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-23 18:10:28 +0100

    Finish WelcomeScreen.java

--- src/screens/WelcomeScreen.java
+++ src/screens/WelcomeScreen.java
@@ -13,5 +13,10 @@ public class WelcomeScreen extends Screen{
         System.out.println("Napisz 1, aby się zalogować.");
         System.out.println("Napisz 2, aby się zarejestrować.");
         System.out.println("Napisz 3, aby zamknąć program.");
+        switch (INPUT.nextInt()){
+            case 1 -> changeScreen(new LoginScreen(hotel));
+            case 2 -> changeScreen(new RegisterScreen(hotel));
+            case 3 -> System.exit(0);
+        }
     }
 }
