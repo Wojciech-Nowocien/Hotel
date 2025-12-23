bee623933705f4dc7acd60317bc51bbd315141b6
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-23 17:52:44 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,10 @@
-void main() {
-    IO.println("Hello world!");
+import model.Hotel;
+import screens.WelcomeScreen;
+
+public class Main {
+    static void main() {
+        Hotel hotel = new Hotel();
+        WelcomeScreen screen = new WelcomeScreen(hotel);
+        hotel.render(screen);
+    }
 }
