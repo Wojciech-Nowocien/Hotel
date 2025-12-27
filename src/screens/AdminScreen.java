7899fd4ccc5b6fcab48d0bcc4e878c5242acab56
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-27 12:47:43 +0100

    Add ability to list all rooms

--- src/screens/AdminScreen.java
+++ src/screens/AdminScreen.java
@@ -1,22 +1,27 @@
 package screens;
 
+import managers.EventManager;
 import managers.RoomManager;
 import model.Room;
 import model.RoomType;
 
 public class AdminScreen extends Screen {
     private final RoomManager rooms;
+    private final EventManager events;
 
     public AdminScreen() {
         this.rooms = hotel.getRooms();
+        this.events = hotel.getEvents();
     }
 
     @Override
     public void render() {
         System.out.println("Panel administratora\n");
         System.out.println("Napisz 1, aby dodać nowy pokój.");
+        System.out.println("Napisz 2, aby wyświetlić wszystkie pokoje.");
         switch (INPUT.nextInt()) {
             case 1 -> addRoom();
+            case 2 -> showAllRooms();
         }
     }
 
@@ -31,5 +36,20 @@ public class AdminScreen extends Screen {
         } catch (IllegalArgumentException e) {
             System.out.println("\nBłąd: Niepoprawny rodzaj pokoju!\n");
         }
+    }
+
+    private void showAllRooms() {
+        if (rooms.getRooms().isEmpty()){
+            System.out.println("Obecnie hotel nie ma żadnych pokoi!\n");
+            return;
+        }
+
+        System.out.println("Lista pokoi:");
+
+        System.out.println("Nr\t\t\t\tTyp\t\t\t\t\tStatus");
+        for (Room r : rooms.getRooms()) {
+            System.out.println(r.getNumber() + "\t\t\t\t" + r.getType() + "\t\t\t" + events.getLastStatus(r));
+        }
+        System.out.println();
     }
 }
