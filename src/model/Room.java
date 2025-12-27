172d1e4cf03feeb1c382f97d9a08cad56940f45c
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-27 10:49:51 +0100

    Add fields to Room.java

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,7 +1,19 @@
 package model;
 
 public class Room {
-    public Room(){
+    private final int number;
+    private final RoomType type;
 
+    public Room(int number, RoomType type) {
+        this.number = number;
+        this.type = type;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public RoomType getType() {
+        return type;
     }
 }
