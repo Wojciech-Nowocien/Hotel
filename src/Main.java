1b0624d898677282ddebdc2a323df4bccee5ca46
Wojciech Nowocie┼ä <wojciech2008.nn@gmail.com>
2025-12-28 11:03:48 +0100

    Initial add fixed data

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import model.Admin;
+import model.Client;
 import model.Hotel;
+import model.RoomType;
 import screens.WelcomeScreen;
 
 void main() {
     Hotel hotel = Hotel.HOTEL;
+    try {
+        hotel.add(new Admin("a", "a"));
+        hotel.add(new Client("c", "c"));
+        hotel.add(RoomType.SINGLE);
+        hotel.add(RoomType.DOUBLE);
+        hotel.add(RoomType.APARTMENT);
+    } catch (Exception _) {
+        throw new RuntimeException("Invalid fixed data!");
+    }
     hotel.render(new WelcomeScreen());
 }
